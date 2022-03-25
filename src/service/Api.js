c445aa4961cabec794ee16459abdedc3f8e87669
pritoo <preetmangal@gmail.com>
2022-03-25 17:19:14 +0530

    Update Api.js

--- src/service/Api.js
+++ src/service/Api.js
@@ -2,6 +2,20 @@ import axios from 'axios'
 
 const url= "http://localhost:3003/users";
  
-export const getUsers = async () => {
-    return await axios.get(url);
+export const getUsers = async (id) => {
+    id = id || '';
+    return await axios.get(`${url}/${id}`);
 }
+
+export const addUsers = async (users) => {
+    return await axios.post(url,users);
+}
+
+export const editUsers = async (id,users) => {
+    return await axios.put(`${url}/${id}`,users);
+}
+
+export const deleteUsers = async (id) => {
+    return await axios.delete(`${url}/${id}`);
+}
+
